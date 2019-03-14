a5e27ed3c49ef9bc4c7443ad5f0a0272c11dcf5c
root <root@vm>
2019-03-14 06:12:03 +0000

    updated tuple and list examples

--- 3.py
+++ 3.py
@@ -1,3 +1,35 @@
+
+# Tuple example
+# You cannot change values in a tuple:
+thistuple = ("apple", "banana", "cherry")
+thistuple[1] = "blackcurrant"
+# The values will remain the same:
+print(thistuple)
+
+##################################################################
+# List example
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+# Change the second item:
+print(thislist)
+
+
+##################################################################
+import numpy
+
+#print numpy.zeros((1,2))                    #Default type is float
+#Output : [[ 0.  0.]] 
+
+#print numpy.zeros((1,2), dtype = numpy.int) #Type changes to int
+
+# tuple use example
+nums = tuple(map(int, input().split()))
+print (numpy.zeros(nums, dtype = numpy.int))
+print (numpy.ones(nums, dtype = numpy.int))
+
+
+##################################################################
+
 """
 x = 1
 y = 2.8
